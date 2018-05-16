c4ecccd65811379306b58c94d3040c169c4f2fb1
Sergey Khmelevskoy <xs290@me.com>
2018-05-16 16:50:53 +0300

    added missing icons

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -8,13 +8,13 @@ var config   = require('../config.js');
 gulp.task('images', function(){
   return gulp
     .src([
-      config.src.img + '/**/*.{jpg,png,jpeg,svg,gif}',
+      config.src.img + '/**/*.*',
       '!' + config.src.img + '/svgo/**/*.*'
     ])
-    .pipe(config.production ? cache(imagemin({interlaced: true})) : util.noop())
+    // .pipe(config.production ? cache(imagemin({interlaced: true})) : util.noop())
     .pipe(gulp.dest(config.dest.img))
 });
 
 gulp.task('images:watch', function() {
-  gulp.watch(config.src.img + '/**/*.{jpg,png,jpeg,svg,gif}', ['images']);
+  gulp.watch(config.src.img + '/**/*.*', ['images']);
 });
