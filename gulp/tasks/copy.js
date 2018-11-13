1b7076d8cec0a630dfe1ec194e31e097fa1322f0
Sergey Khmelevskoy <xs290@me.com>
2018-11-13 22:16:39 +0300

    seo and new video

--- gulp/tasks/copy.js
+++ gulp/tasks/copy.js
@@ -7,10 +7,18 @@ gulp.task('copy:video', function() {
     .pipe(gulp.dest(config.dest.video));
 });
 
+gulp.task('copy:root', function(){
+    return gulp
+      .src(config.src.root + "/*")
+      .pipe(gulp.dest(config.dest.root))
+})
+
 gulp.task('copy', [
+  'copy:root',
   'copy:video',
 ]);
 
 gulp.task('copy:watch', function() {
+  gulp.watch(config.src.root + '/*', ['copy:root']);
   gulp.watch(config.src.video + '/*.*', ['copy:video']);
 });
